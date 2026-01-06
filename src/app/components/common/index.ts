4b9050dde198c7a26246b7e12abaabc1c1fa16b1
Cuong Dev <cuongna.dev@gmail.com>
2026-01-07 03:57:04 +0700

    fix: fix build deploy #3

--- src/app/components/common/index.ts
+++ src/app/components/common/index.ts
@@ -3,6 +3,7 @@ export { default as DetailVideoUpload } from "./upload/DetailVideoUpload";
 export { default as UploadEditDetails } from "./upload/UploadEditDetail";
 export { default as UploadSelectedFile } from "./upload/UploadSelectedFile";
 export { default as VisibilityVideoUpload } from "./upload/VisibilityVideoUpload";
+export { default as OpenCreateChannelListener } from "./channel/OpenCreateChannelListener";
 
 export { VideoGrid } from "./video/videoGrid/VideoGrid";
 export { VideoUploadItem } from "./video/videoItem/VideoUploadItem";
